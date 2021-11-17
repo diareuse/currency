0cc984a4332930cef1f2467f6ce460babc256f19
Viktor De Pasquale <info@depasquale.wiki>
2021-11-17 09:41:18 +0100

    Updated setting calendar to start and end of the day

--- app/src/main/java/org/billthefarmer/currency/domain/rate/ExchangeRatesDatabase.kt
+++ app/src/main/java/org/billthefarmer/currency/domain/rate/ExchangeRatesDatabase.kt
@@ -3,8 +3,9 @@ package org.billthefarmer.currency.domain.rate
 import org.billthefarmer.currency.domain.adapter.ExchangeRatesAdapter
 import org.billthefarmer.currency.domain.database.DaoRate
 import org.billthefarmer.currency.domain.model.ExchangeRate
+import org.billthefarmer.currency.domain.tooling.setToDayEnd
+import org.billthefarmer.currency.domain.tooling.setToDayStart
 import java.util.*
-import java.util.concurrent.TimeUnit
 
 class ExchangeRatesDatabase(
     private val rate: DaoRate,
@@ -12,18 +13,10 @@ class ExchangeRatesDatabase(
 ) : ExchangeRates {
 
     override fun getCurrentRates(): List<ExchangeRate> {
-        val dayStart = getDayStart()
-        val stored = rate.getRatesInRange(dayStart, dayStart + TimeUnit.DAYS.toMillis(1))
+        val dayStart = Calendar.getInstance().setToDayStart().timeInMillis
+        val dayEnd = Calendar.getInstance().setToDayEnd().timeInMillis
+        val stored = rate.getRatesInRange(dayStart, dayEnd)
         return stored.map(adapter::adapt)
-    }
-
-    private fun getDayStart(): Long {
-        return Calendar.getInstance().also {
-            it[Calendar.HOUR_OF_DAY] = 0
-            it[Calendar.MINUTE] = 0
-            it[Calendar.SECOND] = 0
-            it[Calendar.MILLISECOND] = 0
-        }.timeInMillis
     }
 
 }
---
+++ app/src/main/java/org/billthefarmer/currency/domain/tooling/Calendar.kt
@@ -0,0 +1,16 @@
+package org.billthefarmer.currency.domain.tooling
+
+import java.util.*
+
+fun Calendar.setToDayStart() = apply {
+    this[Calendar.HOUR_OF_DAY] = 0
+    this[Calendar.MINUTE] = 0
+    this[Calendar.SECOND] = 0
+    this[Calendar.MILLISECOND] = 0
+}
+
+fun Calendar.setToDayEnd() = apply {
+    setToDayStart()
+    this[Calendar.DAY_OF_YEAR]++
+    this[Calendar.MILLISECOND]--
+}
