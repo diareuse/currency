cf36b769ffa5b34762301842167dc85ee00fe172
Viktor De Pasquale <info@depasquale.wiki>
2021-12-11 13:09:56 +0100

    Added vertical dismiss pattern

---
+++ app/src/main/java/org/billthefarmer/currency/presentation/view/common/DismissDirectionVertical.kt
@@ -0,0 +1,5 @@
+package org.billthefarmer.currency.presentation.view.common
+
+enum class DismissDirectionVertical {
+    Up, Down, Both
+}
---
+++ app/src/main/java/org/billthefarmer/currency/presentation/view/common/DismissVerticallyDefaults.kt
@@ -0,0 +1,9 @@
+package org.billthefarmer.currency.presentation.view.common
+
+import androidx.compose.ui.unit.dp
+
+object DismissVerticallyDefaults {
+    val dampingRatio = 1f / 5
+    val maxOffset = 16.dp
+    val dismissDirection = DismissDirectionVertical.Up
+}
