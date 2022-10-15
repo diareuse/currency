dbe3a1af67361bb097b729e165ae35aa5e4cec27
Viktor De Pasquale <info@depasquale.wiki>
2022-10-15 14:58:01 +0200

    Added failover feature

---
+++ library-core/src/main/java/cursola/network/ExchangeRateRepositoryFailover.kt
@@ -0,0 +1,22 @@
+package cursola.network
+
+import java.util.Currency
+
+class ExchangeRateRepositoryFailover(
+    private val primary: ExchangeRateRepository,
+    private val secondary: ExchangeRateRepository
+) : ExchangeRateRepository {
+
+    override suspend fun get(): List<Currency> {
+        return primary.runCatching { get() }
+            .recover { secondary.get() }
+            .getOrThrow()
+    }
+
+    override suspend fun get(from: Currency, to: Currency): ExchangeRate {
+        return primary.runCatching { get(from, to) }
+            .recover { secondary.get(from, to) }
+            .getOrThrow()
+    }
+
+}
