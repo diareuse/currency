88f0b542cef77f6550bab14b3b54f180ec5b354a
Viktor De Pasquale <info@depasquale.wiki>
2022-10-15 14:27:25 +0200

    Added caching effect

---
+++ library-core/src/main/java/cursola/network/ExchangeRateRepositoryCache.kt
@@ -0,0 +1,43 @@
+package cursola.network
+
+import java.util.Currency
+
+class ExchangeRateRepositoryCache(
+    private val origin: ExchangeRateRepository
+) : ExchangeRateRepository {
+
+    private var list: List<Currency>? = null
+    private var rates: MutableMap<CurrencyKey, ExchangeRate> = mutableMapOf()
+
+    override suspend fun get(): List<Currency> {
+        return list ?: origin.get().also {
+            list = it
+        }
+    }
+
+    override suspend fun get(from: Currency, to: Currency): ExchangeRate {
+        return rates.getOrPut(CurrencyKey(from, to)) {
+            origin.get(from, to)
+        }
+    }
+
+    private class CurrencyKey(
+        vararg val currencies: Currency
+    ) {
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (other !is CurrencyKey) return false
+
+            if (!currencies.contentEquals(other.currencies)) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            return currencies.contentHashCode()
+        }
+
+    }
+
+}
---
+++ library-core/src/test/java/cursola/network/ExchangeRateRepositoryCacheTest.kt
@@ -0,0 +1,69 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
+package cursola.network
+
+import cursola.network.util.nextCurrency
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoMoreInteractions
+import org.mockito.kotlin.whenever
+import kotlin.test.assertSame
+
+class ExchangeRateRepositoryCacheTest {
+
+    private lateinit var origin: ExchangeRateRepository
+    private lateinit var repo: ExchangeRateRepositoryCache
+
+    @Before
+    fun prepare() {
+        origin = mock()
+        repo = ExchangeRateRepositoryCache(origin)
+    }
+
+    @Test
+    fun getList_callsSource() = runTest {
+        repo.get()
+        verify(origin).get()
+    }
+
+    @Test
+    fun getList_returns_cachedItem() = runTest {
+        whenever(origin.get()).thenReturn(listOf(nextCurrency()))
+        val firstResult = repo.get()
+        val nextResult = repo.get()
+        assertSame(firstResult, nextResult)
+    }
+
+    @Test
+    fun getRate_callsSource() = runTest {
+        val first = nextCurrency()
+        val second = nextCurrency()
+        repo.get(first, second)
+        verify(origin).get(first, second)
+    }
+
+    @Test
+    fun getRate_returns_cachedItem() = runTest {
+        val first = nextCurrency()
+        val second = nextCurrency()
+        repo.get(first, second)
+        verify(origin).get(first, second)
+        repo.get(first, second)
+        verifyNoMoreInteractions(origin)
+    }
+
+    @Test
+    fun getRate_callsSource_withReversedOrder() = runTest {
+        val first = nextCurrency()
+        val second = nextCurrency()
+        repo.get(first, second)
+        verify(origin).get(first, second)
+        repo.get(second, first)
+        verify(origin).get(second, first)
+    }
+
+}
