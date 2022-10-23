dd49812fbc727b7da4e921f7cd8dc092a9aebc83
Viktor De Pasquale <info@depasquale.wiki>
2022-10-23 14:57:36 +0200

    Updated app dependencies

--- app/src/main/java/wiki/depasquale/currency/screen/style/Shapes.kt
+++ app/src/main/java/wiki/depasquale/currency/screen/style/Shapes.kt
@@ -1,28 +1,13 @@
 package wiki.depasquale.currency.screen.style
 
-import androidx.compose.foundation.shape.CornerBasedShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
-val ShapeSmall = RoundedCornerShape(8.dp)
-val ShapeMedium = RoundedCornerShape(12.dp)
-val ShapeLarge = RoundedCornerShape(32.dp)
-
-data class Shapes(
-    val small: CornerBasedShape,
-    val medium: CornerBasedShape,
-    val large: CornerBasedShape
+val CurrencyShapes = Shapes(
+    extraSmall = RoundedCornerShape(4.dp),
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(12.dp),
+    large = RoundedCornerShape(32.dp),
+    extraLarge = RoundedCornerShape(0.dp),
 )
-
-object DefaultShapes {
-
-    val shapes = Shapes(
-        small = ShapeSmall,
-        medium = ShapeMedium,
-        large = ShapeLarge
-    )
-
-}
-
-val MaterialTheme.shapes get() = DefaultShapes.shapes
