9163740ad815707c207fe91a11717f97885564da
Viktor De Pasquale <info@depasquale.wiki>
2022-10-23 18:02:54 +0200

    Added app entry point

--- app/src/main/java/wiki/depasquale/currency/App.kt
+++ app/src/main/java/wiki/depasquale/currency/App.kt
@@ -2,7 +2,9 @@ package wiki.depasquale.currency
 
 import android.app.Application
 import com.google.android.material.color.DynamicColors
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class App : Application() {
 
     override fun onCreate() {
