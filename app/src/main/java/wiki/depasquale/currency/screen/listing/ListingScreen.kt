43cfd3677d54f0dfa8214baf32faa62c39a75126
Viktor De Pasquale <info@depasquale.wiki>
2022-10-24 19:16:53 +0200

    Added ime padding to listing screen

--- app/src/main/java/wiki/depasquale/currency/screen/listing/ListingScreen.kt
+++ app/src/main/java/wiki/depasquale/currency/screen/listing/ListingScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -101,7 +102,10 @@ private fun ListingScreen(
                     isFavorite = it.isFavorite
                 )
             }
-            item { Spacer(Modifier.navigationBarsPadding()) }
+            item { Spacer(
+                Modifier
+                    .navigationBarsPadding()
+                    .imePadding()) }
         }
     }
 }
