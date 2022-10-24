7c23add585201a32c9ae4db824e959ca13198751
Viktor De Pasquale <info@depasquale.wiki>
2022-10-24 19:03:33 +0200

    Updated input fields

--- app-view/src/main/java/cursola/view/CurrencyInput.kt
+++ app-view/src/main/java/cursola/view/CurrencyInput.kt
@@ -1,33 +1,32 @@
 package cursola.view
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import java.util.Currency
 import java.util.Locale
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CurrencyInput(
     value: String,
@@ -37,38 +36,34 @@ fun CurrencyInput(
     onDone: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    BasicTextField(
-        modifier = modifier,
+    TextField(
+        modifier = modifier.fillMaxWidth(),
         value = value,
+        onValueChange = onValueChanged,
         textStyle = LocalTextStyle.current.copy(
             textAlign = TextAlign.End,
             fontWeight = FontWeight.ExtraBold
         ),
-        onValueChange = {
-            onValueChanged(it)
-        },
-        decorationBox = {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterHorizontally),
-                verticalAlignment = Alignment.Bottom
-            ) {
-                Text(
-                    currency.getSymbol(locale),
-                    style = LocalTextStyle.current.copy(
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight.ExtraBold
-                    )
-                )
-                Spacer(modifier = Modifier.weight(1f))
-                it()
-            }
-        },
+        colors = TextFieldDefaults.textFieldColors(
+            containerColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        maxLines = 1,
         keyboardActions = KeyboardActions(onDone = { onDone() }),
         keyboardOptions = KeyboardOptions(
             imeAction = ImeAction.Done,
             keyboardType = KeyboardType.Number
-        )
+        ),
+        leadingIcon = {
+            Text(
+                currency.getSymbol(locale),
+                style = LocalTextStyle.current.copy(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+        }
     )
 }
 
--- app/src/main/java/wiki/depasquale/currency/screen/listing/ListingScreen.kt
+++ app/src/main/java/wiki/depasquale/currency/screen/listing/ListingScreen.kt
@@ -3,22 +3,25 @@ package wiki.depasquale.currency.screen.listing
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import cursola.rate.view.ConvertedCurrency
@@ -51,12 +54,22 @@ private fun ListingScreen(
     onNavigateBack: () -> Unit,
     locale: Locale = Locale.getDefault()
 ) {
-    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    var filter by remember { mutableStateOf("") }
     Scaffold(
         topBar = {
             TopAppBar(
-                modifier = Modifier.statusBarsPadding(),
-                title = {},
+                title = {
+                    TextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = filter,
+                        onValueChange = { filter = it },
+                        colors = TextFieldDefaults.textFieldColors(
+                            containerColor = Color.Transparent,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent
+                        )
+                    )
+                },
                 navigationIcon = {
                     IconButton(onClick = onNavigateBack) {
                         Icon(
@@ -64,21 +77,18 @@ private fun ListingScreen(
                             contentDescription = null
                         )
                     }
-                },
-                colors = TopAppBarDefaults.smallTopAppBarColors(
-                    containerColor = Color.Transparent,
-                    scrolledContainerColor = Color.Transparent
-                ),
-                scrollBehavior = scrollBehavior
+                }
             )
         }
     ) { padding ->
         LazyColumn(
-            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
             contentPadding = padding + PaddingValues(24.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            items(items, key = ConvertedCurrency::currency) {
+            items(
+                items.filter { it.name(locale).contains(filter, ignoreCase = true) },
+                key = ConvertedCurrency::currency
+            ) {
                 ListingItem(
                     onAddItem = { onAddItem(it.currency) },
                     onRemoveItem = { onRemoveItem(it.currency) },
