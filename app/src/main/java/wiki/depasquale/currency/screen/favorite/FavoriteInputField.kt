0cbbdb3241e26b74525dbc565b685b2ad8d643fd
Viktor De Pasquale <info@depasquale.wiki>
2022-10-24 18:42:50 +0200

    Fixed color in dark mode

--- app/src/main/java/wiki/depasquale/currency/screen/favorite/FavoriteInputField.kt
+++ app/src/main/java/wiki/depasquale/currency/screen/favorite/FavoriteInputField.kt
@@ -1,9 +1,12 @@
 package wiki.depasquale.currency.screen.favorite
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,21 +24,40 @@ fun FavoriteInputField(
     onCloseKeyboard: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    ProvideTextStyle(value = MaterialTheme.typography.displaySmall) {
-        CurrencyInput(
-            modifier = modifier.padding(24.dp),
-            value = value,
-            onValueChanged = onValueChanged,
-            currency = selected,
-            locale = locale,
-            onDone = onCloseKeyboard
-        )
+    CompositionLocalProvider(LocalContentColor provides MaterialTheme.colorScheme.onSurface) {
+        ProvideTextStyle(
+            value = MaterialTheme.typography.displaySmall.copy(
+                color = LocalContentColor.current
+            )
+        ) {
+            CurrencyInput(
+                modifier = modifier.padding(24.dp),
+                value = value,
+                onValueChanged = onValueChanged,
+                currency = selected,
+                locale = locale,
+                onDone = onCloseKeyboard
+            )
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun Preview() {
+    CurrencyTheme {
+        FavoriteInputField(
+            value = "0.0",
+            selected = Currency.getAvailableCurrencies().random(),
+            locale = Locale.getDefault(),
+            onValueChanged = {},
+            onCloseKeyboard = { /*TODO*/ })
+    }
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+private fun Preview_Night() {
     CurrencyTheme {
         FavoriteInputField(
             value = "0.0",
