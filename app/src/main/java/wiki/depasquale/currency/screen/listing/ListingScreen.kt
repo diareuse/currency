4cff89881aeda10f560282941e6406c4b2f88744
Viktor De Pasquale <info@depasquale.wiki>
2022-10-25 19:24:12 +0200

    Added clearing search field

--- app/src/main/java/wiki/depasquale/currency/screen/listing/ListingScreen.kt
+++ app/src/main/java/wiki/depasquale/currency/screen/listing/ListingScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -14,9 +13,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -25,7 +21,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import cursola.rate.view.ConvertedCurrency
@@ -63,18 +58,9 @@ private fun ListingScreen(
         topBar = {
             TopAppBar(
                 title = {
-                    TextField(
-                        modifier = Modifier.fillMaxWidth(),
-                        value = filter,
-                        onValueChange = { filter = it },
-                        colors = TextFieldDefaults.textFieldColors(
-                            containerColor = Color.Transparent,
-                            focusedIndicatorColor = Color.Transparent,
-                            unfocusedIndicatorColor = Color.Transparent
-                        ),
-                        placeholder = {
-                            Text("Search here…")
-                        }
+                    ListingSearchField(
+                        filter = filter,
+                        onFilterChanged = { filter = it }
                     )
                 },
                 navigationIcon = {
@@ -106,10 +92,13 @@ private fun ListingScreen(
                     isFavorite = it.isFavorite
                 )
             }
-            item { Spacer(
-                Modifier
-                    .navigationBarsPadding()
-                    .imePadding()) }
+            item {
+                Spacer(
+                    Modifier
+                        .navigationBarsPadding()
+                        .imePadding()
+                )
+            }
         }
     }
 }
---
+++ app/src/main/java/wiki/depasquale/currency/screen/listing/ListingSearchField.kt
@@ -0,0 +1,45 @@
+package wiki.depasquale.currency.screen.listing
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import wiki.depasquale.currency.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListingSearchField(
+    filter: String,
+    onFilterChanged: (String) -> Unit
+) {
+    val style = LocalTextStyle.current
+    TextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = filter,
+        onValueChange = onFilterChanged,
+        colors = TextFieldDefaults.textFieldColors(
+            containerColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        placeholder = {
+            Text("Search here…", style = style)
+        },
+        trailingIcon = {
+            if (filter.isBlank())
+                Icon(painterResource(R.drawable.ic_search), null)
+            else
+                IconButton(onClick = { onFilterChanged("") }) {
+                    Icon(painterResource(R.drawable.ic_clear), null)
+                }
+        }
+    )
+}
