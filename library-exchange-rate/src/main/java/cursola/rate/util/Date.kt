1d956f1f1aab85c577763e3501954d9d7e4bd27a
Viktor De Pasquale <info@depasquale.wiki>
2022-10-25 19:57:56 +0200

    Added faster loading from today

--- library-exchange-rate/src/main/java/cursola/rate/util/Date.kt
+++ library-exchange-rate/src/main/java/cursola/rate/util/Date.kt
@@ -5,6 +5,11 @@ import java.util.Date
 
 val Date.isToday: Boolean
     get() {
+        return this in todayRange
+    }
+
+val todayRange: ClosedRange<Date>
+    get() {
         val calendar = Calendar.getInstance()
         calendar.set(Calendar.MILLISECOND, 0)
         calendar.set(Calendar.SECOND, 0)
@@ -14,5 +19,5 @@ val Date.isToday: Boolean
         calendar.add(Calendar.DAY_OF_YEAR, 1)
         calendar.add(Calendar.MILLISECOND, -1)
         val end = calendar.time
-        return this in start..end
+        return start..end
     }
