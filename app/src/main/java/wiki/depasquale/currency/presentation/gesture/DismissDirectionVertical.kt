ceaa1fd12a59e9eb185612d27ebf60aa0ab21bd2
Viktor De Pasquale <info@depasquale.wiki>
2022-10-26 19:45:57 +0200

    Removed unnecessary files

--- app/src/main/java/wiki/depasquale/currency/presentation/gesture/DismissDirectionVertical.kt
+++
@@ -1,5 +0,0 @@
-package wiki.depasquale.currency.presentation.gesture
-
-enum class DismissDirectionVertical {
-    Up, Down, Both
-}
--- app/src/main/java/wiki/depasquale/currency/presentation/gesture/DismissVerticallyDefaults.kt
+++
@@ -1,9 +0,0 @@
-package wiki.depasquale.currency.presentation.gesture
-
-import androidx.compose.ui.unit.dp
-
-object DismissVerticallyDefaults {
-    val dampingRatio = 1f / 5
-    val maxOffset = 16.dp
-    val dismissDirection = DismissDirectionVertical.Up
-}
