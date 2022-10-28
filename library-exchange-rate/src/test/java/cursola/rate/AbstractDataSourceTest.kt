ff327e0b4fc36a052a2949128b8c040fe98e025b
Viktor De Pasquale <info@depasquale.wiki>
2022-10-28 13:36:01 +0200

    Removed performance stubs

--- library-exchange-rate/src/test/java/cursola/rate/AbstractDataSourceTest.kt
+++ library-exchange-rate/src/test/java/cursola/rate/AbstractDataSourceTest.kt
@@ -1,11 +1,9 @@
 package cursola.rate
 
 import cursola.rate.analytics.AnalyticService
-import cursola.rate.analytics.PerformanceService
 import cursola.rate.database.ExchangeRateDatabase
 import cursola.rate.network.ExchangeRateService
 import cursola.rate.storage.Storage
-import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
@@ -17,7 +15,6 @@ internal abstract class AbstractDataSourceTest {
     protected lateinit var database: ExchangeRateDatabase
     protected lateinit var network: ExchangeRateService
     protected lateinit var analytics: AnalyticService
-    protected lateinit var performance: PerformanceService
 
     @Suppress("RedundantUnitExpression")
     @Before
@@ -29,23 +26,6 @@ internal abstract class AbstractDataSourceTest {
         whenever(database.rates()).thenReturn(mock())
         whenever(database.favorites()).thenReturn(mock())
         whenever(analytics.log(any(), any())).then { Unit }
-        runBlocking {
-            whenever(performance.network(any(), any(), any<suspend () -> Any>())).then {
-                runBlocking {
-                    it.getArgument<suspend () -> Any>(2).invoke()
-                }
-            }
-        }
-        runBlocking {
-            whenever(performance.trace(any(), any<suspend () -> Any>())).then {
-                runBlocking {
-                    it.getArgument<suspend () -> Any>(1).invoke()
-                }
-            }
-        }
-        whenever(performance.traceInline(any(), any<() -> Any>())).then {
-            it.getArgument<() -> Any>(1).invoke()
-        }
         mockStorage()
         prepare()
     }
