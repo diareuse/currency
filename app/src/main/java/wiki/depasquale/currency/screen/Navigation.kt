e4b3ab4fd62f77709caf95a74fd5b9f5cf50646f
Viktor De Pasquale <info@depasquale.wiki>
2022-10-28 14:14:13 +0200

    Added transition animations

--- app/src/main/java/wiki/depasquale/currency/screen/Navigation.kt
+++ app/src/main/java/wiki/depasquale/currency/screen/Navigation.kt
@@ -1,18 +1,28 @@
 package wiki.depasquale.currency.screen
 
+import androidx.compose.animation.AnimatedContentScope.SlideDirection
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavDeepLink
+import androidx.navigation.NavGraphBuilder
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import wiki.depasquale.currency.screen.favorite.FavoriteScreen
 import wiki.depasquale.currency.screen.listing.ListingScreen
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun Navigation() {
-    val controller = rememberNavController()
-    NavHost(controller, "/favorites") {
-        composable("/favorites") {
+    val controller = rememberAnimatedNavController()
+    AnimatedNavHost(controller, "/favorites") {
+        composableCard("/favorites") {
             FavoriteScreen(
                 viewModel = hiltViewModel(),
                 onNavigateToListing = {
@@ -20,7 +30,7 @@ fun Navigation() {
                 }
             )
         }
-        composable("/listing") {
+        composableCard("/listing") {
             ListingScreen(
                 viewModel = hiltViewModel(),
                 onNavigateBack = controller::navigateUp
@@ -28,3 +38,28 @@ fun Navigation() {
         }
     }
 }
+
+@OptIn(ExperimentalAnimationApi::class)
+private fun NavGraphBuilder.composableCard(
+    route: String,
+    arguments: List<NamedNavArgument> = emptyList(),
+    deepLinks: List<NavDeepLink> = emptyList(),
+    content: @Composable (AnimatedVisibilityScope.(NavBackStackEntry) -> Unit)
+) = composable(
+    route = route,
+    arguments = arguments,
+    deepLinks = deepLinks,
+    enterTransition = {
+        slideIntoContainer(SlideDirection.Left)
+    },
+    exitTransition = {
+        slideOutOfContainer(SlideDirection.Left, targetOffset = { it / 2 }) + fadeOut()
+    },
+    popEnterTransition = {
+        slideIntoContainer(SlideDirection.Right, initialOffset = { it / 2 }) + fadeIn()
+    },
+    popExitTransition = {
+        slideOutOfContainer(SlideDirection.Right)
+    },
+    content = content
+)
