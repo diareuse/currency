cc3c5399479ee0901d8bd0fd38bd166ae09cb122
Viktor De Pasquale <info@depasquale.wiki>
2022-10-28 19:17:51 +0200

    Added refreshing rates every day while in the background

--- app/src/main/java/wiki/depasquale/currency/App.kt
+++ app/src/main/java/wiki/depasquale/currency/App.kt
@@ -1,15 +1,25 @@
 package wiki.depasquale.currency
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class App : Application() {
+class App : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
 
     override fun onCreate() {
         super.onCreate()
         DynamicColors.applyToActivitiesIfAvailable(this)
     }
+
+    override fun getWorkManagerConfiguration() = Configuration.Builder()
+        .setWorkerFactory(workerFactory)
+        .build()
 
 }
