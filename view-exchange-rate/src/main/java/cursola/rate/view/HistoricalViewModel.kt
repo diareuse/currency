ea7eb229ba4ae642fa1e661a87706e4829f4c464
Viktor De Pasquale <info@depasquale.wiki>
2022-10-30 14:56:18 +0100

    Updated historical values to return pertially loaded data early

--- view-exchange-rate/src/main/java/cursola/rate/view/HistoricalViewModel.kt
+++ view-exchange-rate/src/main/java/cursola/rate/view/HistoricalViewModel.kt
@@ -5,12 +5,12 @@ import androidx.lifecycle.viewModelScope
 import cursola.rate.ExchangeRate
 import cursola.rate.FavoriteDataSource
 import cursola.rate.HistoryDataSource
-import cursola.rate.di.ScopeSinceInception
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import java.util.Currency
 import java.util.Date
@@ -19,17 +19,29 @@ import javax.inject.Inject
 @HiltViewModel
 class HistoricalViewModel @Inject internal constructor(
     private val favorite: FavoriteDataSource,
-    @ScopeSinceInception
     private val history: HistoryDataSource
 ) : ViewModel() {
 
-    val items = flow { emit(favorite.list()) }
-        .distinctUntilChanged()
+    private val favorites = flow { emit(favorite.list()) }
+
+    private val emptyValues = favorites
+        .map(::toEmptyHistoryValues)
+
+    private val actualValues = favorites
         .map(::toHistoryValues)
+        .onStart { emit(emptyList()) }
+
+    val items = combine(emptyValues, actualValues) { empty, actual ->
+        actual.ifEmpty { empty }
+    }
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
 
     private suspend fun toHistoryValues(currencies: List<Currency>): List<HistoryValues> {
         return currencies.map { HistoryValues(it, history.get(it).map(::ChartValue)) }
+    }
+
+    private fun toEmptyHistoryValues(currencies: List<Currency>): List<HistoryValues> {
+        return currencies.map { HistoryValues(it, emptyList()) }
     }
 
     data class HistoryValues(
