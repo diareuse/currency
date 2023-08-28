bdb4bb1c22d9e9d375811dd9105079de9c29a96d
Viktor De Pasquale <hello@depasquale.wiki>
2023-08-28 16:48:44 +0200

    Updated navigation

--- app/src/main/java/wiki/depasquale/currency/screen/Navigation.kt
+++ app/src/main/java/wiki/depasquale/currency/screen/Navigation.kt
@@ -2,27 +2,19 @@
 
 package wiki.depasquale.currency.screen
 
-import android.annotation.SuppressLint
-import androidx.compose.animation.*
-import androidx.compose.animation.AnimatedContentTransitionScope.*
-import androidx.compose.animation.core.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.windowsizeclass.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
-import androidx.compose.ui.platform.*
 import androidx.compose.ui.unit.*
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
-import androidx.navigation.NamedNavArgument
-import androidx.navigation.NavBackStackEntry
-import androidx.navigation.NavDeepLink
-import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.createGraph
+import cursola.rate.view.FavoritesViewModel
+import cursola.rate.view.HistoricalViewModel
+import cursola.rate.view.ListingViewModel
 import cursola.view.VerticalDivider
 import wiki.depasquale.currency.screen.favorite.FavoriteScreen
 import wiki.depasquale.currency.screen.history.HistoryScreen
@@ -30,47 +22,50 @@ import wiki.depasquale.currency.screen.listing.ListingScreen
 
 @Composable
 fun Navigation(size: WindowSizeClass) {
-    val controller = rememberNavController()
-    val lifecycleOwner = LocalLifecycleOwner.current
-    val viewModelStoreOwner = checkNotNull(LocalViewModelStoreOwner.current)
-    controller.setLifecycleOwner(lifecycleOwner)
-    controller.setViewModelStore(viewModelStoreOwner.viewModelStore)
-    // todo fix transitions between screen sizes
     when (size.widthSizeClass) {
-        WindowWidthSizeClass.Compact -> NavigationMobile(controller)
-        WindowWidthSizeClass.Medium -> NavigationMedium(controller)
-        WindowWidthSizeClass.Expanded -> NavigationExpanded(controller)
+        WindowWidthSizeClass.Compact -> NavigationMobile()
+        WindowWidthSizeClass.Medium,
+        WindowWidthSizeClass.Expanded -> NavigationExpanded()
     }
 }
 
 @Composable
-fun NavigationMobile(controller: NavHostController) {
+fun NavigationMobile(
+    controller: NavHostController = rememberNavController()
+) {
     NavHost(
-        controller, "/favorites",
-        enterTransition = { slideInHorizontally { it } },
-        exitTransition = { fadeOut() + slideOutHorizontally() },
-        popEnterTransition = { fadeIn() + slideInHorizontally() },
-        popExitTransition = { slideOutHorizontally { it } }
+        navController = controller,
+        startDestination = "/favorites"
     ) {
-        composableCard("/favorites") { FavoriteScreen(controller) }
-        composableCard("/favorites/history") { HistoryScreen(controller) }
-        composableCard("/listing") { ListingScreen(controller) }
+        composable("/favorites") {
+            FavoriteScreen(
+                onNavigateToHistory = { controller.navigate("/favorites/history") },
+                onNavigateToListing = { controller.navigate("/listing") },
+            )
+        }
+        composable("/favorites/history") { HistoryScreen(onNavigateBack = controller::navigateUp) }
+        composable("/listing") { ListingScreen(onNavigateBack = controller::navigateUp) }
     }
 }
 
 @Composable
-fun NavigationMedium(controller: NavHostController) {
-    val startDestination = "/favorites/history"
-    val graph = remember(startDestination) {
-        controller.createGraph(startDestination, null) {
-            composableCard("/favorites/history") { HistoryScreen(controller) }
-            composableCard("/listing") { ListingScreen(controller) }
-        }.also {
-            controller.graph = it
+fun NavigationExpanded(
+    mainController: NavHostController = rememberNavController(),
+    detailController: NavHostController = rememberNavController()
+) {
+    Row(modifier = Modifier.fillMaxSize()) {
+        NavHost(
+            modifier = Modifier.weight(1f),
+            navController = mainController,
+            startDestination = "/favorites"
+        ) {
+            composable("/favorites") {
+                FavoriteScreen(
+                    onNavigateToHistory = { detailController.navigate("/favorites/history") },
+                    onNavigateToListing = { detailController.navigate("/listing") },
+                )
+            }
         }
-    }
-    Row {
-        FavoriteScreen(modifier = Modifier.weight(1f), controller = controller)
         VerticalDivider(
             modifier = Modifier
                 .padding(vertical = 16.dp)
@@ -79,110 +74,64 @@ fun NavigationMedium(controller: NavHostController) {
         )
         NavHost(
             modifier = Modifier.weight(1f),
-            graph = graph,
-            navController = controller,
-            enterTransition = { slideInHorizontally { it } },
-            exitTransition = { fadeOut() + slideOutHorizontally() },
-            popEnterTransition = { fadeIn() + slideInHorizontally() },
-            popExitTransition = { slideOutHorizontally { it } }
-        )
-    }
-}
-
-@Composable
-fun NavigationExpanded(controller: NavHostController) {
-    remember {
-        controller.createGraph("/favorites/history", null) {
-            composableCard("/favorites/history") {}
-        }.also {
-            controller.graph = it
+            navController = detailController,
+            startDestination = "/none"
+        ) {
+            composable("/none") {}
+            composable("/favorites/history") { HistoryScreen(onNavigateBack = detailController::navigateUp) }
+            composable("/listing") { ListingScreen(onNavigateBack = detailController::navigateUp) }
         }
-    }
-    Row(modifier = Modifier.fillMaxSize()) {
-        FavoriteScreen(modifier = Modifier.weight(1f), controller = controller)
-        VerticalDivider(
-            modifier = Modifier
-                .padding(vertical = 16.dp)
-                .imePadding()
-                .navigationBarsPadding()
-        )
-        HistoryScreen(modifier = Modifier.weight(1f), controller = controller)
-        VerticalDivider(
-            modifier = Modifier
-                .padding(vertical = 16.dp)
-                .imePadding()
-                .navigationBarsPadding()
-        )
-        ListingScreen(modifier = Modifier.weight(1f), controller = controller)
     }
 }
 
 @Composable
 private fun FavoriteScreen(
-    controller: NavHostController,
+    onNavigateToListing: () -> Unit,
+    onNavigateToHistory: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val viewModel = hiltViewModel<FavoritesViewModel>()
+    val items by viewModel.items.collectAsState(initial = emptyList())
+    val selected by viewModel.selected.collectAsState()
+    val value by viewModel.value.collectAsState()
     FavoriteScreen(
         modifier = modifier,
-        viewModel = hiltViewModel(),
-        onNavigateToListing = navigateTask(controller, "/listing"),
-        onNavigateToHistory = navigateTask(controller, "/favorites/history")
+        items = items,
+        selected = selected,
+        value = value,
+        onValueChanged = { viewModel.value.value = it },
+        onItemSelected = { viewModel.selected.value = it.currency },
+        onNavigateToListing = onNavigateToListing,
+        onNavigateToHistory = onNavigateToHistory,
     )
 }
 
 @Composable
 private fun ListingScreen(
-    controller: NavHostController,
+    onNavigateBack: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val viewModel = hiltViewModel<ListingViewModel>()
+    val items by viewModel.items.collectAsState(initial = emptyList())
     ListingScreen(
         modifier = modifier,
-        viewModel = hiltViewModel(),
-        onNavigateBack = navigateUpTask(controller, "/listing")
+        items = items,
+        onAddItem = viewModel::add,
+        onRemoveItem = viewModel::remove,
+        onNavigateBack = onNavigateBack
     )
 }
 
 @Composable
 private fun HistoryScreen(
-    controller: NavHostController,
+    onNavigateBack: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val viewModel = hiltViewModel<HistoricalViewModel>()
+    val items by viewModel.items.collectAsState()
     HistoryScreen(
+        items = items,
         modifier = modifier,
-        viewModel = hiltViewModel(),
-        onNavigateBack = navigateUpTask(controller, "/favorites/history")
+        onNavigateBack = onNavigateBack
     )
 }
-
-private fun navigateTask(
-    controller: NavHostController,
-    route: String
-): (() -> Unit)? {
-    if (controller.graph.none { it.route == route }) return null
-    if (controller.currentDestination?.route == route) return null
-    return { controller.navigate(route) }
-}
-
-@SuppressLint("RestrictedApi")
-private fun navigateUpTask(controller: NavHostController, fromRoute: String): (() -> Unit)? {
-    if (controller.graph.none { it.route == fromRoute }) return null
-    if (
-        controller.currentBackStack.value.asSequence()
-            .map { it.destination.route }
-            .filterNot { it.isNullOrBlank() || it == fromRoute }
-            .count() == 0
-    ) return null
-    return { controller.navigateUp() }
-}
-
-private fun NavGraphBuilder.composableCard(
-    route: String,
-    arguments: List<NamedNavArgument> = emptyList(),
-    deepLinks: List<NavDeepLink> = emptyList(),
-    content: @Composable (AnimatedVisibilityScope.(NavBackStackEntry) -> Unit)
-) = composable(
-    route = route,
-    arguments = arguments,
-    deepLinks = deepLinks,
-    content = content
-)
--- app/src/main/java/wiki/depasquale/currency/screen/history/HistoryScreen.kt
+++ app/src/main/java/wiki/depasquale/currency/screen/history/HistoryScreen.kt
@@ -1,39 +1,19 @@
 package wiki.depasquale.currency.screen.history
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.*
+import androidx.compose.material3.*
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.res.*
+import androidx.compose.ui.text.style.*
+import androidx.compose.ui.tooling.preview.*
+import androidx.compose.ui.unit.*
 import cursola.rate.view.HistoricalViewModel
 import cursola.view.topFadingEdge
 import wiki.depasquale.currency.R
@@ -41,24 +21,12 @@ import wiki.depasquale.currency.screen.favorite.copy
 import wiki.depasquale.currency.screen.favorite.plus
 import wiki.depasquale.currency.screen.style.CurrencyTheme
 import java.util.Currency
-
-@Composable
-fun HistoryScreen(
-    viewModel: HistoricalViewModel,
-    onNavigateBack: (() -> Unit)?,
-    modifier: Modifier = Modifier,
-) {
-    val items by viewModel.items.collectAsState()
-    HistoryScreen(
-        modifier = modifier,
-        items = items,
-        onNavigateBack = onNavigateBack
-    )
-}
+import java.util.Date
+import kotlin.random.Random.Default.nextDouble
 
 @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
-private fun HistoryScreen(
+fun HistoryScreen(
     items: List<HistoricalViewModel.HistoryValues>,
     onNavigateBack: (() -> Unit)?,
     modifier: Modifier = Modifier,
@@ -128,11 +96,16 @@ private fun HistoryScreen(
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
 @Composable
 private fun Preview(
     items: List<HistoricalViewModel.HistoryValues> = Currency.getAvailableCurrencies().take(10)
-        .map { HistoricalViewModel.HistoryValues(it, emptyList()) }
+        .map {
+            HistoricalViewModel.HistoryValues(
+                it,
+                List(5) { HistoricalViewModel.ChartValue(Date(), nextDouble()) })
+        }
 ) {
     CurrencyTheme {
         HistoryScreen(
