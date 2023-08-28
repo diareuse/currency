8c7187656a1942dcf51d62925e3cc45691be7c5a
Viktor De Pasquale <hello@depasquale.wiki>
2023-08-28 17:32:49 +0200

    Fixed DI issues

--- library-exchange-rate/src/debug/java/cursola/rate/di/PlatformModule.kt
+++ library-exchange-rate/src/debug/java/cursola/rate/di/PlatformModule.kt
@@ -1,20 +1,21 @@
-package wiki.depasquale.currency.di
+package cursola.rate.di
 
+import android.util.Log
 import cursola.rate.analytics.AnalyticService
 import cursola.rate.analytics.PerformanceService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.components.SingletonComponent
 
 @Module
-@InstallIn(ActivityRetainedComponent::class)
+@InstallIn(SingletonComponent::class)
 class PlatformModule {
 
     @Provides
     fun analytics() = object : AnalyticService {
         override fun log(event: String, params: Map<String, Any>) {
-            /* no-op */
+            Log.v("Analytics", "Event(name=$event,params=$params)")
         }
     }
 
@@ -26,15 +27,24 @@ class PlatformModule {
             method: String,
             body: suspend () -> T
         ): T {
-            return body()
+            Log.v("Performance", "($method:$url) start")
+            return body().also {
+                Log.v("Performance", "($method:$url) stop")
+            }
         }
 
         override suspend fun <T> trace(marker: String, body: suspend () -> T): T {
-            return body()
+            Log.v("Performance", "($marker) start")
+            return body().also {
+                Log.v("Performance", "($marker) stop")
+            }
         }
 
         override fun <T> traceInline(marker: String, body: () -> T): T {
-            return body()
+            Log.v("Performance", "($marker) start")
+            return body().also {
+                Log.v("Performance", "($marker) stop")
+            }
         }
 
     }
--- library-exchange-rate/src/release/java/cursola/rate/di/PlatformModule.kt
+++ library-exchange-rate/src/release/java/cursola/rate/di/PlatformModule.kt
@@ -1,6 +1,5 @@
-package wiki.depasquale.currency.di
+package cursola.rate.di
 
-import android.util.Log
 import cursola.rate.analytics.AnalyticService
 import cursola.rate.analytics.PerformanceService
 import dagger.Module
@@ -15,36 +14,39 @@ class PlatformModule {
     @Provides
     fun analytics() = object : AnalyticService {
         override fun log(event: String, params: Map<String, Any>) {
-            Log.v("Analytics", "Event(name=$event,params=$params)")
+            //Firebase.analytics.logEvent(event, bundleOf(pairs = params.toList().toTypedArray()))
         }
     }
 
     @Provides
     fun performance() = object : PerformanceService {
 
+        //private val performance = Firebase.performance
+
         override suspend fun <T> network(
             url: String,
             method: String,
             body: suspend () -> T
         ): T {
-            Log.v("Performance", "($method:$url) start")
-            return body().also {
-                Log.v("Performance", "($method:$url) stop")
-            }
+            /*val metric = performance.newHttpMetric(url, method)
+            metric.start()
+            try {*/
+            return body()
+            /*} finally {
+                metric.stop()
+            }*/
         }
 
         override suspend fun <T> trace(marker: String, body: suspend () -> T): T {
-            Log.v("Performance", "($marker) start")
-            return body().also {
-                Log.v("Performance", "($marker) stop")
-            }
+            //return performance.newTrace(marker).trace {
+            return body()
+            //}
         }
 
         override fun <T> traceInline(marker: String, body: () -> T): T {
-            Log.v("Performance", "($marker) start")
-            return body().also {
-                Log.v("Performance", "($marker) stop")
-            }
+            //return performance.newTrace(marker).trace {
+            return body()
+            //}
         }
 
     }
